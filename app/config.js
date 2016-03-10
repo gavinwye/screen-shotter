dc3fd79f7579d2ab8035af0517cb7fdd243eef3d
Adam Conder <adamdconder@gmail.com>
2016-03-10 13:37:16 +0000

    updating config

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Service name goes here",
+  serviceName: "Child Benefit",
 
   // Default port that prototype runs on
   port: '3000',
