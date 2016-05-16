a814fea1d9418cc3b488b867b9735bc89a9438d0
Adam Fenwick <adam.fenwick@digital.hmrc.gov.uk>
2016-05-16 14:42:01 +0100

    Fixed issue with overwriting routes functions

--- app/routes.js
+++ app/routes.js
@@ -15,57 +15,19 @@ app.use(expressSession({secret:'somesecrettokenhere'}));
 
 app.use(bodyParser());
 
-router.get('/', function (req, res) {
-
-  var redirectUrl = config.host(req);
-  // this is used for the IV (authentication) prototype so that we dont have to keep
-  // changing the URl between localhost and the heroku app's URL.
-  // The js for this is in the file /app/views/config.js
-
-  res.render('index', {
-    'redirect' : redirectUrl
-  });
-});
-
-
-// Example routes - feel free to delete these
-
-// Passing data into a page
-router.get('/examples/template-data', function (req, res) {
-
-  res.render('examples/template-data', { 'name' : 'Foo' });
-
-});
-
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18;
-
-  if (over18 == "false"){
-
-    // redirect to the relevant page
-    res.redirect("/examples/under-18");
-
-  } else {
-
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18');
-
-  }
-
-});
-
-module.exports = router;
-
 module.exports = {
   bind : function (router) {
 
-    // router.get('/', function (req, res) {
-    //   res.render('index');
-    // });
+    router.get('/', function (req, res) {
+      var redirectUrl = config.host(req);
+      // this is used for the IV (authentication) prototype so that we dont have to keep
+      // changing the URl between localhost and the heroku app's URL.
+      // The js for this is in the file /app/views/config.js
+
+      res.render('index', {
+        'redirect' : redirectUrl
+      });
+    });
 
   require('./routes-filelist')(router);
   require('./custom-routes')(router);
