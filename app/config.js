45059d50b35987f34fbf47382ecf062c608ce5a2
Adam Fenwick <adam.fenwick@digital.hmrc.gov.uk>
2016-05-16 14:58:41 +0100

    Hostname changed

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ var util = require("util");
 
 var config = {
 
-  hostname: "cb-test-proto",
+  hostname: "cb-prototype",
 
   // Service name used in header. Eg: 'Renew your passport'
   serviceName: "Child Benefit ",
